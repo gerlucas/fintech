28b8d2f18abcd52962205a3ee06ed074c2891c4c
George Lucas <georgelucasgit@gmail.com>
2023-09-12 13:22:07 -0300

    Criação do projeto

---
+++ pages/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overmind</title>
+</head>
+
+<body>
+    <main>
+        <h1>Overmind</h1>
+
+        <ul>
+
+            <?php
+            $paginas = [
+                'entrada.php' => 'Simulação',
+                'historico.php' => 'Historico'
+            ];
+
+            $link = '<li><a href="%s">%s<a/></li>';
+
+            foreach ($paginas as $pagina => $titulo) {
+                printf($link, $pagina, $titulo);
+            }
+            ?>
+
+        </ul>
+    </main>
+
+    <footer>
+        <p>George Lucas e Gabriel&copy; 2023</p>
+    </footer>
+</body>
+
+</html>
